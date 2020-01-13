3a1a9051f858814f5b3720055edca4bff0ba2e12
danbrud <dannybrudner@users.noreply.github.com>
2020-01-13 17:20:57 +0200

    added lesson plan

---
+++ main.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sayHello = function(){
+//   console.log("Hello Danny")
+// }
+
+// sayHello()
+
+// const sayHello = function (name) {
+//   console.log("Hello " + name)
+// }
+
+// // sayHello("Jona")
+
+// const names = ["Daniel", "Mariana", "Efrat", "Gili"]
+
+// // for (let name of names) {
+// //   sayHello(name)
+// // }
+
+// const displayAge = function (name, age) {
+//   console.log("My name is " + name + " and I am " + age + " years old.")
+// }
+
+// // displayAge("Corinne", 29)
+
+// const growOld = function (user) {
+//   user.age++
+// }
+
+// const user = { name: "corinne", age: 29 }
+// // growOld()
+// // console.log(user)
+
+
+// const getSum = function (x, y) {
+//   return x + y
+// }
+
+// let sum = getSum(10, 10)
+// // console.log(sum)
+
+// const findLargest = function (arr) {
+//   let largest = arr[0]
+
+//   for (let v of arr) {
+//     if (v > largest) {
+//       largest = v
+//     }
+//   }
+
+//   return largest
+// }
+
+// const arr = [1, 15, 19, 31, 2, 10, 3]
+
+// const largest = findLargest(arr)
+// // console.log(largest)
+
+// const person = {
+//   name: "Danny",
+//   speak: function () {
+//     console.log("Hello there")
+//   }
+// }
+
+// person.speak()
