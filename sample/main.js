5c58d36b1a359c6036abc2053e8ffa3db1a34ffb
danbrud <dannybrudner@users.noreply.github.com>
2020-01-15 14:43:28 +0200

    added lesson plan

---
+++ sample/main.js
@@ -0,0 +1,55 @@
+
+
+// let num = 8
+
+// console.log(num)
+
+// num = 10
+
+// console.log(num)
+
+// const number = 9
+
+// console.log(number)
+
+// // const number = 100
+
+// console.log(number)
+
+// // number = number +10
+
+// let name = 'danny'
+
+// let bool = false
+
+let num = 10
+
+// console.log(num  =  num * 2)
+
+// num /= 10
+
+// console.log(num)
+
+let name = 'danny'
+
+let lastName = 'Brudner'
+
+// console.log(name * num)
+
+// console.log(!('danny' === 'str' || 7 === 8))
+
+const userName = 'danbrudy'
+
+let isLoggedIn = false
+
+let money = 110
+
+// let shapira = null
+
+if (isLoggedIn && money > 15) {
+  console.log('hello rich guy')
+} else if (isLoggedIn) {
+  console.log('hello ' + userName)
+} else {
+  console.log('pay me now')
+}
