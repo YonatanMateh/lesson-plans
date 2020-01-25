57bb1d6fbbf5606fd3b8a3c2d7be161bd4da2cda
danbrud <dannybrudner@users.noreply.github.com>
2020-01-25 20:09:23 +0200

    added lesson plan

---
+++ main.js
@@ -0,0 +1,22 @@
+
+//click on remove - we need to know data-id of current post
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//click submit - get the value of what's inside the input
+
---
+++ sample/main.js
@@ -0,0 +1,25 @@
+//click on remove - we need to know data-id
+
+/* $(".remove").on("click", function () {
+    alert($(".post").data().id)
+}) */
+
+
+
+
+//click on remove - traverse upward
+
+// $(".remove").on("click", function () {
+//     alert($(this).closest(".post").data().id)
+// })
+
+
+
+
+
+//click submit - get the value of what's inside the input
+
+// $('button').on('click', function () {
+//     let relevantInputValue = $(this).closest("div").find("input").val()
+//     alert(relevantInputValue)
+// })
