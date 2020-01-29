63e8fedb48fae1911593896583da4e460609c627
danbrud <dannybrudner@users.noreply.github.com>
2020-01-29 13:19:39 +0200

    completed lesson plan

--- main.js
+++ main.js
@@ -43,18 +43,24 @@
 
 
 
+
 // let flights
 // $('button').on('click', function () {
 //   const input = $('input').val()
 //   console.log(input)
 // })
 
+// $('button').on('click', function () {
+
+  // const input = $('input').val()
+  //   console.log(input)
 // $.get(`https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3`, function (response) {
 //   // flights = response
 //   console.log(response)
 //   const price = response.data[0].price
 //   const duration = response.data[0].fly_duration
 //   $('body').append(`<p>Flight costs ${price}</p><p>Flights duration is ${duration}</p>`)
+// })
 // })
 
 // console.log(flights)
